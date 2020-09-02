fc63db4b7e64e08ff1bbed2bc8512f1838b97b19
zy_user <zhuoya_wang@126.com>
2020-09-02 19:16:07 +0800

    concurrentHashMap多个操作加锁

--- src/main/java/org/geekbang/time/commonmistakes/concurrenttool/concurrenthashmapmisuse/ConcurrentHashMapMisuseController.java
+++ src/main/java/org/geekbang/time/commonmistakes/concurrenttool/concurrenthashmapmisuse/ConcurrentHashMapMisuseController.java
@@ -14,6 +14,10 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 
+
+/**
+ * concurrentHashMap只能保证提供的原子性读写操作是线程安全的
+ */
 @RestController
 @RequestMapping("concurrenthashmapmisuse")
 @Slf4j
@@ -22,6 +26,14 @@ public class ConcurrentHashMapMisuseController {
     private static int THREAD_COUNT = 10;
     private static int ITEM_COUNT = 1000;
 
+
+    /**
+     * rangeClosed(begin,end),输出从begin到end（包括begin与end），间隔为1的数
+     * range(begin,end),输出从begin到end（包括begin，不包括end），间隔为1的数
+     *
+     * boxed 将基本类型流变为其包装器流
+     *
+     */
     private ConcurrentHashMap<String, Long> getData(int count) {
         return LongStream.rangeClosed(1, count)
                 .boxed()
@@ -29,11 +41,22 @@ public class ConcurrentHashMapMisuseController {
                         (o1, o2) -> o1, ConcurrentHashMap::new));
     }
 
+
+    /**
+     * 在使用concurrentHashMap时，不代表它的多个操作之间状态是一致的，是没有其他线程在同时操作。如果需要保证的话，要加锁。
+     * 例如 size、isEmpty、containsValue等聚合方法，在并发情况下会反应conCurrentHashMap的中间状态。所以在并发情况下，这些方法只可以参考，而不可以控制流程
+     * putAll这样的聚合方法，也不能确保原子性，在putAll的过程中，有可能获取到部分数据
+     *
+     * @return
+     * @throws InterruptedException
+     */
     @GetMapping("wrong")
     public String wrong() throws InterruptedException {
+        //concurrentHashMap中先放入900个数
         ConcurrentHashMap<String, Long> concurrentHashMap = getData(ITEM_COUNT - 100);
         log.info("init size:{}", concurrentHashMap.size());
 
+        //分别启用10个线程计算 还差多少个数到100.把数据补进去
         ForkJoinPool forkJoinPool = new ForkJoinPool(THREAD_COUNT);
         forkJoinPool.execute(() -> IntStream.rangeClosed(1, 10).parallel().forEach(i -> {
             int gap = ITEM_COUNT - concurrentHashMap.size();
@@ -47,6 +70,11 @@ public class ConcurrentHashMapMisuseController {
         return "OK";
     }
 
+    /**
+     * 针对上面的错错代码，对整段concurrentHashMap进行操作的代码段加锁
+     * @return
+     * @throws InterruptedException
+     */
     @GetMapping("right")
     public String right() throws InterruptedException {
         ConcurrentHashMap<String, Long> concurrentHashMap = getData(ITEM_COUNT - 100);
