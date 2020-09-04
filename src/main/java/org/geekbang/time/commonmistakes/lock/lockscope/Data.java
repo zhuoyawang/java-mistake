88ccf07f984d1d5cafe326a3563c2516282453e5
zy_user <zhuoya_wang@126.com>
2020-09-04 20:07:39 +0800

    锁保护的资源

--- src/main/java/org/geekbang/time/commonmistakes/lock/lockscope/Data.java
+++ src/main/java/org/geekbang/time/commonmistakes/lock/lockscope/Data.java
@@ -12,10 +12,22 @@ class Data {
         return counter;
     }
 
+    /**
+     * synchronized 加给方法只能保证方法不会被多个线程同时执行
+     * 在方法上加synchronized只能确保多个线程无法执行同一个实例的wrong方法，却不能保证不会执行不同实例的wrong方法。
+     * 静态的counter在多个实例中共享，所以必然会出现线程安全问题。
+     *
+     * 静态字段要用静态方法（可以直接用类名.方法的那种）来保护
+     */
     public synchronized void wrong() {
         counter++;
     }
 
+    /**
+     * 修改方法一、同样在类中定义一个静态的object,在操作counter之前对这个字段加锁。
+     *
+     * 修改方法二、将方法设置为静态的，因为会改变原来的代码结构，所以不采纳
+     */
     public void right() {
         synchronized (locker) {
             counter++;
--- src/main/java/org/geekbang/time/commonmistakes/lock/lockscope/LockScopeController.java
+++ src/main/java/org/geekbang/time/commonmistakes/lock/lockscope/LockScopeController.java
@@ -11,6 +11,13 @@ import java.util.stream.IntStream;
 @RestController
 @RequestMapping("lockscope")
 @Slf4j
+
+/**
+ * 加锁前要清楚锁和被保护的对象是不是一个层面的
+ * 静态字段属于类，类级别的锁才可以保护，非静态字段字段属于类实例，实例级别的锁就可以保护。
+ * 在使用锁之前一定要先清楚，我们保护的逻辑是什么，多线程执行的情况下应该如何保护。
+ *
+ */
 public class LockScopeController {
 
     @GetMapping("wrong")
@@ -27,6 +34,12 @@ public class LockScopeController {
         return Data.getCounter();
     }
 
+
+    /**
+     * 两个线程会交错执行add 和compare的业务逻辑，而业务逻辑不是原子性的操作
+     * a++和b++会穿梭在compare中,a<b也不是原子性的操作，需要先取出A的操作，再取出B的操作，然后进行比较。
+     * 所有的过程都不是原子性的，所以不能保证结果的正确性
+     */
     @GetMapping("wrong2")
     public String wrong2() {
         Interesting interesting = new Interesting();
@@ -35,6 +48,9 @@ public class LockScopeController {
         return "OK";
     }
 
+    /**
+     * 需要给两个方法全部都加上锁才可以
+     */
     @GetMapping("right2")
     public String right2() {
         Interesting interesting = new Interesting();
